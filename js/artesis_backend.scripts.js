3dbc091a3555b3c1b4ed962098492335e4815ada
Anatoly Muntean <am@inlead.dk>
2013-04-18 14:27:16 +0300

    case 2058 - Moved the updating login box logic here.

---
+++ js/artesis_backend.scripts.js
@@ -0,0 +1,14 @@
+/**
+ * @file
+ *
+ * Backend functionality.
+ */
+(function ($) {
+
+  // Ajax responder for updating login box.
+  Drupal.ajax.prototype.commands['artesis_backend_update_login'] = function (ajax, response, status) {
+    $('.block-ding-user-frontend-ding-username h2').html(Drupal.t("Welcome") + '<span>' + $('.block-ding-user-frontend-ding-username #ding-user-name .content').text() + '</span>');
+    return;
+  };
+
+})(jQuery);
